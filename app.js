dd6ef19c5e4bc67979a58eb1632de240e3e5cdd3
quangvinh0513 <vinhnq@zigexn.vn>
2016-09-26 06:28:29 +0700

    init assignment 1

---
+++ app.js
@@ -0,0 +1,44 @@
+(function () {
+  'use strict';
+
+  angular.module('ShoppingListCheckOff', [])
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService)
+  .controller('ToBuyShoppingController',
+              ['ShoppingListCheckOffService', ToBuyShoppingController])
+  .controller('AlreadyBoughtShoppingController',
+              ['ShoppingListCheckOffService', AlreadyBoughtShoppingController])
+
+
+  function ShoppingListCheckOffService() {
+    var service = this
+
+    var toBuyItems = ["10 cookies", "12 eggs", "13 apples", "14 cats", "15 dimensions"]
+    var bought = []
+
+    service.buy = function(index, item) {
+      toBuy.splice(index, 1)
+      bought.push(item)
+    }
+
+    service.getToBuy = function() {
+      return toBuy
+    }
+
+    service.getBought = function() {
+      return bought
+    }
+  }
+
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    this.toBuy = ShoppingListCheckOffService.getToBuy();
+
+    this.buy = function(index, item) {
+      ShoppingListCheckOffService.buy(index, item);
+    }
+  }
+
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    this.bought = ShoppingListCheckOffService.getBought();
+  }
+
+})();
